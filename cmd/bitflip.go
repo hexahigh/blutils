546c8b47ca91aefacca1928aefd5616ae99dca8b
Boofdev <hexahigh0@gmail.com>
2024-06-01 14:03:02 +0200

    bitflip will now save if it recieves a sigint

--- cmd/bitflip.go
+++ cmd/bitflip.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"math/big"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/spf13/cobra"
 )
@@ -49,30 +51,45 @@ var bitflipCmd = &cobra.Command{
 		}
 
 		bitsToFlip := *bitflipParams.BitsToFlip
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
 		for i := 0; i < bitsToFlip; i++ {
-			// Choose a random position
-			pos, err := rand.Int(rand.Reader, maxPos)
-			if err != nil {
-				log.Fatalf("Failed to generate random number: %v", err)
-			}
-
-			if *bitflipParams.MinOffset != 0 && pos.Int64() < int64(*bitflipParams.MinOffset) {
-				i--
-				continue
-			}
-
-			// Perform bit flip
-			fileContent[pos.Int64()] ^= 1
-
-			if *bitflipParams.ChunkSize > 1 && pos.Int64()+int64(*bitflipParams.ChunkSize) < maxPos.Int64() {
-				for j := 0; j < *bitflipParams.ChunkSize; j++ {
-					fileContent[pos.Int64()+int64(j)] ^= 1
+			select {
+			case <-sigChan:
+				verbosePrintln(2, "Interrupt received, saving file...")
+				err = os.WriteFile(filename, fileContent, 0644)
+				if err != nil {
+					verbosePrintln(0, "Failed to save file:", err)
+				} else {
+					verbosePrintln(2, "File saved successfully.")
 				}
-			}
+				os.Exit(0)
+			default:
+				pos, err := rand.Int(rand.Reader, maxPos)
+				if err != nil {
+					log.Fatalf("Failed to generate random number: %v", err)
+				}
 
-			verbosePrintln(3, "Flipped at offset", pos.Int64())
+				if *bitflipParams.MinOffset != 0 && pos.Int64() < int64(*bitflipParams.MinOffset) {
+					i-- // Decrement counter to retry this iteration
+					continue
+				}
+
+				// Perform bit flip
+				fileContent[pos.Int64()] ^= 1
+
+				if *bitflipParams.ChunkSize > 1 && pos.Int64()+int64(*bitflipParams.ChunkSize) < maxPos.Int64() {
+					for j := 0; j < *bitflipParams.ChunkSize; j++ {
+						fileContent[pos.Int64()+int64(j)] ^= 1
+					}
+				}
+
+				verbosePrintln(3, "Flipped at offset", pos.Int64())
+			}
 		}
 
+		// Save file after all bits have been flipped
 		err = os.WriteFile(filename, fileContent, 0644)
 		if err != nil {
 			verbosePrintln(0, "Failed to save file:", err)
